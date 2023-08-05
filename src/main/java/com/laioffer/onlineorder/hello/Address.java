e51ca755c109f9b95e4266b85532f2840de7d05b
JunhaoDeng <48834693+JunhaoDeng@users.noreply.github.com>
2023-08-06 05:25:33 +0800

    test

---
+++ src/main/java/com/laioffer/onlineorder/hello/Address.java
@@ -0,0 +1,10 @@
+package com.laioffer.onlineorder.hello;
+
+public record Address(
+        String street,
+        String city,
+        String state,
+        String country
+) {
+}
+
---
+++ src/main/java/com/laioffer/onlineorder/hello/HelloController.java
@@ -0,0 +1,33 @@
+package com.laioffer.onlineorder.hello;
+
+
+
+
+import com.github.javafaker.Faker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Person hello(@RequestParam(required = true, defaultValue = "en_US") String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String name = faker.name().fullName();
+        String company = faker.company().name();
+        String street = faker.address().streetAddress();
+        String city = faker.address().city();
+        String state = faker.address().state();
+        String bookTitle = faker.book().title();
+        String bookAuthor = faker.book().author();
+
+//        String template = "This is %s\n" +
+//                "I work at %s\n" +
+//                "I live at %s in %s %s\n" +
+//                "My favorite book is %s by %s\n";
+        return new Person(name, company, new Address(street, city, state, null), new Book(bookTitle, bookAuthor));
+    }
+}
