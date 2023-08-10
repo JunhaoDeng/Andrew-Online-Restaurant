e70e0fcde8e351623f8da0b223ba4c5f6ddcc5f6
JunhaoDeng <48834693+JunhaoDeng@users.noreply.github.com>
2023-08-10 15:47:09 +0800

    service Implementation

---
+++ src/main/java/com/laioffer/onlineorder/repository/MenuItemRepository.java
@@ -0,0 +1,15 @@
+package com.laioffer.onlineorder.repository;
+
+
+import com.laioffer.onlineorder.entity.MenuItemEntity;
+import org.springframework.data.repository.ListCrudRepository;
+
+
+import java.util.List;
+
+
+public interface MenuItemRepository extends ListCrudRepository<MenuItemEntity, Long> {
+
+
+    List<MenuItemEntity> getByRestaurantId(Long restaurantId);
+}
---
+++ src/main/java/com/laioffer/onlineorder/repository/RestaurantRepository.java
@@ -0,0 +1,9 @@
+package com.laioffer.onlineorder.repository;
+
+
+import com.laioffer.onlineorder.entity.RestaurantEntity;
+import org.springframework.data.repository.ListCrudRepository;
+
+
+public interface RestaurantRepository extends ListCrudRepository<RestaurantEntity, Long> {
+}
